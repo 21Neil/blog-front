2b03a11a4bfee5c0ce2911635efe52dcc537e963
21Neil <88711171+21Neil@users.noreply.github.com>
2026-01-16 15:20:19 +0800

    feat: turn navbar to fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import { Flex, Title } from '@mantine/core';
 import { Link } from 'react-router';
 import style from './Navbar.module.css';
+import { useWindowScroll } from '@mantine/hooks';
 
 const Navbar = ({ home }) => {
+  const [scroll] = useWindowScroll();
+
   return (
     <Flex
       c='white'
       justify='space-between'
       align='center'
-      className={style.navbar + ' ' + (home && style.navbarTransparent)}
+      className={
+        style.navbar +
+        ' ' +
+        (home && style.homeNavbar) +
+        ' ' +
+        (home && scroll.y < 339 && style.navbarTransparent)
+      }
+      top={0}
+      w='100%'
     >
       <Title ff='caveat'>
         <Link to='/'>Neil.</Link>
