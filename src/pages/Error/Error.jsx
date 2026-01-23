b0c8d6fd062c0dfb433363d03921328e9a512c69
21Neil <88711171+21Neil@users.noreply.github.com>
2026-01-23 12:03:21 +0800

    feat: change error page layout

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,7 +2,7 @@ import { Stack, Title } from "@mantine/core";
 
 const Error = () => {
   return (
-    <Stack component='main' justify="center" align="center">
+    <Stack component='main' justify="center" align="center" h='100vh'>
       <Title>404 Not found</Title>
     </Stack>
   )
